385c7b0872455c55b3978bc803aa83d07a68a034
adrianzevenster <64969874+adrianzevenster@users.noreply.github.com>
2025-05-06 20:06:04 +0200

    local[roles], project binding to members and roles

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+locals {
+  roles = [
+  "roles/documentai.apiUser",
+  "roles/documentai.editor",
+  "roles/aiplatform.user",
+  "roles/iam.serviceAccountUser",
+  "roles/storage.objectViewer",
+  "roles/storage.objectCreator",
+  "roles/notebooks.viewer",
+  "roles/notebooks.runner",
+  "roles/monitoring.viewer"]
+}
+
+resource "google_project_iam_binding" "project_roles" {
+  for_each = toset(local.roles)
+
+  project  = var.project_id
+  role     = each.key
+
+  members  = vars.members
+}
